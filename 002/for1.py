ee49f30b77a303e95e139ad111298980a77a3c47
root <root@localhost.localdomain>
2016-07-12 13:53:40 +0800

    python 2016 base

---
+++ 002/for1.py
@@ -0,0 +1,7 @@
+    for x in range(0,10):
+        if x >=3:
+            is_break = False
+            break
+        print('x={0}'.format(x))
+    if is_break:
+        break
---
+++ 002/guess.py
@@ -0,0 +1,13 @@
+numer = 5
+i = 1
+print("This guess a number for you!")
+int(input('a'))
+while i > 3:
+    int(input("please input numer"))
+    i+=1
+    if x == 5:
+        print("congratulations ! you win!")
+    else:
+        continue
+else:
+    print("Oops! you fail!")
